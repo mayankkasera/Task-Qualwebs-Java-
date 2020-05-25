39d44dea48145a35d25a58628ee287218e963ced
mayankkasera <mayankkasera98@gmail.com>
2020-05-25 21:33:44 +0530

    Init.

---
+++ app/src/main/java/com/example/task_qualwebs_java/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.task_qualwebs_java;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
